55a1b6fb6eb4000d922486d3db755406caab4048
Jessie <jdeleon@village88.com>
2018-10-10 08:57:38 +0800

    functions inside class

--- airplane 1942/assets/js/main.ts
+++ airplane 1942/assets/js/main.ts
@@ -1,19 +1,46 @@
 let new_player: any;
+let startPlay: any;
 
 window.onload = function(){
 
 	class PlaySettings{
-		public playSettings(element_id:any, tile:any, index:any, action:any, classname:any) {
+		playSettings(element_id:any, tile:any, index:any, action:any, classname:any) {
 			if(action == "add") {
 				document.getElementById(element_id).children[tile].children[index].classList.add(classname);
 			}else if(action == "remove") {
 				document.getElementById(element_id).children[tile].children[index].classList.remove(classname);
 			}
 		}
-		public playSound(audioId:any){
+		playSound(audioId:any){
 			let audioPlayer: HTMLMediaElement = <HTMLAudioElement>document.getElementById(audioId);
 			audioPlayer.load();
 			audioPlayer.play();
+		}
+		spawnEnemy(){
+			setInterval(function(){ 
+				var rand: number = Math.floor((Math.random() * 10) + 1);
+				var	column: number = rand -1;
+				var playS = new PlaySettings();
+				playS.playSettings("col-" + column, 0, 0, "add", "active");
+			}, 1000);
+		}
+		enemyAttack(){
+			setInterval(function(){ 
+				var enemies = document.getElementsByClassName("enemy active");
+				let temp_enemies = Object.assign({}, enemies);
+
+				var num:any;
+				for (num in temp_enemies) {
+
+					if(num <= 8)
+					{
+						temp_enemies[num].classList.remove("active")
+
+						if(temp_enemies[num].parentElement.nextElementSibling.children[0].className.indexOf("player") == -1)
+							temp_enemies[num].parentElement.nextElementSibling.children[0].classList.add("active")
+					}
+				}
+			}, 2000);
 		}
 	}
 
@@ -21,7 +48,7 @@ window.onload = function(){
 		playerName: string;
 		xGrid: number = 10;
 		yGrid: number = 10;
-		position: number = 5;
+		position: number = 2;
 		keyCode(key: any){
 			var currentPos = this.position;
 			var column	   = "col-";
@@ -85,40 +112,18 @@ window.onload = function(){
 		}
 	}
 
+	// Start Game
 	new_player = new Player();
-	var playaudio  = new PlaySettings();
+	startPlay = new PlaySettings();
+
+	// Start AI movements
+	startPlay.spawnEnemy();
+	startPlay.enemyAttack();
 	
+	//Start Sounds
 	setTimeout(function(){
-		playaudio.playSound("audio_bg");
+		startPlay.playSound("audio_bg");
 	}, 500)
-
-
-	//AI movements spawn
-	setInterval(function(){ 
-		var rand: number = Math.floor((Math.random() * 10) + 1);
-		var	column: number = rand -1;
-		var playS = new PlaySettings();
-		playS.playSettings("col-" + column, 0, 0, "add", "active");
-		// document.getElementById('col-'+column).children[0].children[0].classList.add("active");
-	}, 1000);
-
-	// Speed of enemy going down
-	setInterval(function(){ 
-		var enemies = document.getElementsByClassName("enemy active");
-		let temp_enemies = Object.assign({}, enemies);
-
-		var num:any;
-		for (num in temp_enemies) {
-
-			if(num <= 8)
-			{
-				temp_enemies[num].classList.remove("active")
-
-				if(temp_enemies[num].parentElement.nextElementSibling.children[0].className.indexOf("player") == -1)
-					temp_enemies[num].parentElement.nextElementSibling.children[0].classList.add("active")
-			}
-		}
-	}, 2000);
 }
 
 document.addEventListener("keydown", function(event){
--- airplane 1942/dist/assets/js/main.js
+++ airplane 1942/dist/assets/js/main.js
@@ -1,50 +1,93 @@
 "use strict";
 let new_player;
+let startPlay;
 window.onload = function () {
-    class Player {
-        constructor(firstName, lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
+    class PlaySettings {
+        playSettings(element_id, tile, index, action, classname) {
+            if (action == "add") {
+                document.getElementById(element_id).children[tile].children[index].classList.add(classname);
+            }
+            else if (action == "remove") {
+                document.getElementById(element_id).children[tile].children[index].classList.remove(classname);
+            }
+        }
+        playSound(audioId) {
+            let audioPlayer = document.getElementById(audioId);
+            audioPlayer.load();
+            audioPlayer.play();
+        }
+        spawnEnemy() {
+            setInterval(function () {
+                var rand = Math.floor((Math.random() * 10) + 1);
+                var column = rand - 1;
+                var playS = new PlaySettings();
+                playS.playSettings("col-" + column, 0, 0, "add", "active");
+            }, 1000);
+        }
+        enemyAttack() {
+            setInterval(function () {
+                var enemies = document.getElementsByClassName("enemy active");
+                let temp_enemies = Object.assign({}, enemies);
+                var num;
+                for (num in temp_enemies) {
+                    if (num <= 8) {
+                        temp_enemies[num].classList.remove("active");
+                        if (temp_enemies[num].parentElement.nextElementSibling.children[0].className.indexOf("player") == -1)
+                            temp_enemies[num].parentElement.nextElementSibling.children[0].classList.add("active");
+                    }
+                }
+            }, 2000);
+        }
+    }
+    class Player extends PlaySettings {
+        constructor() {
+            super(...arguments);
             this.xGrid = 10;
             this.yGrid = 10;
             this.position = 2;
-            this.playerName = firstName + ' ' + lastName;
         }
         keyCode(key) {
             var currentPos = this.position;
+            var column = "col-";
             if (key == 37 || key == 39 || key == 32) {
                 if (key == 37 && currentPos > 0) { // move left
                     this.position--;
-                    document.getElementById('col-' + currentPos).children[this.xGrid - 1].children[0].classList.remove("active"); // remove class active to plager
-                    document.getElementById('col-' + this.position).children[this.xGrid - 1].children[0].classList.add("active"); // add class active to player
+                    this.playSettings(column + currentPos, this.xGrid - 1, 0, "remove", "active"); // remove class active to plager
+                    this.playSettings(column + this.position, this.xGrid - 1, 0, "add", "active"); // add class active to player
                 }
                 else if (key == 39 && currentPos < this.yGrid - 1) { // move right
                     this.position++;
-                    document.getElementById('col-' + currentPos).children[this.xGrid - 1].children[0].classList.remove("active"); // remove class active to plager
-                    document.getElementById('col-' + this.position).children[this.xGrid - 1].children[0].classList.add("active"); // add class active to player
+                    this.playSettings(column + currentPos, this.xGrid - 1, 0, "remove", "active"); // remove class active to plager
+                    this.playSettings(column + this.position, this.xGrid - 1, 0, "add", "active"); // add class active to player
                 }
                 else if (key == 32) { // attack
+                    this.playSound("audio_attack");
                     var tile = this.xGrid - 2;
                     var max_playground = this.xGrid - 2;
+                    // variable for setinterval to use
+                    let playS = this.playSettings;
+                    let playSound = this.playSound;
                     var interval = setInterval(function () {
-                        if (tile < max_playground)
-                            document.getElementById('col-' + currentPos).children[tile + 1].children[1].classList.remove("active");
-                        document.getElementById('col-' + currentPos).children[tile].children[1].classList.add("active");
+                        if (tile < max_playground) {
+                            playS(column + currentPos, tile + 1, 1, "remove", "active");
+                        }
+                        playS(column + currentPos, tile, 1, "add", "active");
                         if (document.getElementById('col-' + currentPos).children[tile].children[0].className.indexOf("active") != -1) {
                             //Enemy detected, destroy enemy
-                            document.getElementById('col-' + currentPos).children[tile].children[0].classList.remove("active");
-                            document.getElementById('col-' + currentPos).children[tile].children[1].classList.add("impact");
+                            playS(column + currentPos, tile, 0, "remove", "active");
+                            playS(column + currentPos, tile, 1, "add", "impact");
                             setTimeout(function () {
-                                document.getElementById('col-' + currentPos).children[tile + 1].children[1].classList.remove("impact");
+                                playS(column + currentPos, tile + 1, 1, "remove", "impact");
                             }, 500);
                             //remove attack when colided with enemy
                             clearInterval(interval);
-                            document.getElementById('col-' + currentPos).children[tile].children[1].classList.remove("active");
+                            playS(column + currentPos, tile, 1, "remove", "active");
+                            playSound("audio_explosion");
                         }
                         if (tile == 0) {
                             clearInterval(interval);
                             setTimeout(function () {
-                                document.getElementById('col-' + currentPos).children[0].children[1].classList.remove("active");
+                                playS(column + currentPos, 0, 1, "remove", "active");
                             }, 500);
                         }
                         else {
@@ -55,25 +98,16 @@ window.onload = function () {
             }
         }
     }
-    new_player = new Player("Jessie", "de Leon");
-    //AI movements spawn
-    setInterval(function () {
-        var rand = Math.floor((Math.random() * 10) + 1);
-        var test = rand - 1;
-        document.getElementById('col-' + test).children[0].children[0].classList.add("active");
-    }, 1000);
-    // Speed of enemy going down
-    setInterval(function () {
-        var enemies = document.getElementsByClassName("enemy active");
-        let temp_enemies = Object.assign({}, enemies);
-        for (var num in temp_enemies) {
-            if (num <= 8) {
-                temp_enemies[num].classList.remove("active");
-                if (temp_enemies[num].parentElement.nextElementSibling.children[0].className.indexOf("player") == -1)
-                    temp_enemies[num].parentElement.nextElementSibling.children[0].classList.add("active");
-            }
-        }
-    }, 2000);
+    // Start Game
+    new_player = new Player();
+    startPlay = new PlaySettings();
+    // Start AI movements
+    startPlay.spawnEnemy();
+    startPlay.enemyAttack();
+    //Start Sounds
+    setTimeout(function () {
+        startPlay.playSound("audio_bg");
+    }, 500);
 };
 document.addEventListener("keydown", function (event) {
     new_player.keyCode(event.keyCode);
